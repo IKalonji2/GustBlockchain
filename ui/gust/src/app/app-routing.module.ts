20c8433f4587ae2217927c03257a1589f6925e6a
fweni <fweni@student.wethinkcode.co.za>
2024-10-05 16:08:20 +0200

    added ui navigation routes

--- ui/gust/src/app/app-routing.module.ts
+++ ui/gust/src/app/app-routing.module.ts
@@ -1,10 +1,38 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './home/home.component';
+import { LearnComponent } from './learn/learn.component';
+import { DeveloperComponent } from './developer/developer.component';
+import { NetworkComponent } from './network/network.component';
+import { RegisterComponent } from './register/register.component';
+
+const routes: Routes = [
+  {
+    path: "",
+    component: HomeComponent
+  },
+  {
+    path:"about",
+    component: LearnComponent
+  },
+  {
+    path: "developer",
+    component: DeveloperComponent
+  },
+  {
+    path:"network",
+    component: NetworkComponent
+  },
+  {
+    path: "register",
+    component: RegisterComponent
+  }
+];
+
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {bindToComponentInputs: true})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
